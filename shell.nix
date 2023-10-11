8645fe1b9cb138839d86672232aece3ac33db92f
diamondburned <diamond@arikawa-hi.me>
2023-10-11 06:06:26 -0700

    Shrink Nix shell

--- shell.nix
+++ shell.nix
@@ -1,22 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-	fyne = pkgs.callPackage ./nix/fyne.nix {};
-	fyneDeps = import ./nix/fyne-deps.nix { inherit pkgs; };
-in
+{
+	sources ? import ./nix/sources.nix {},
+	pkgs ? import sources.nixpkgs {},
+}:
 
 pkgs.mkShell {
-	buildInputs = with pkgs; [
-		pkgsCross.mingwW64.buildPackages.gcc
-	] ++ fyneDeps.buildInputs;
-
 	nativeBuildInputs = with pkgs; [
-		fyne
 		niv
-		go
-		gopls
-		gotools
-	] ++ fyneDeps.nativeBuildInputs;
-
-	CGO_ENABLED = "1";
+	];
 }
